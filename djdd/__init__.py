88657e7efb06cf52b8f2afee5fcb3e3fbe0b1447
Will Hardy <will@willhardy.com.au>
2014-10-24 01:40:08 +0200

    Cleaning up, using schroot

--- djdd/__init__.py
+++ djdd/__init__.py
@@ -1,3 +1,8 @@
 
-with open('VERSION', 'rb') as f:
+import os
+
+version_file = os.path.join(os.path.dirname(__file__), "VERSION")
+with open(version_file, 'rb') as f:
     __version__ = f.read().decode('utf-8').strip()
+
+from .initialize_build import initialize_build_directory
--- djdd/initialize_build.py
+++ djdd/initialize_build.py
@@ -1,66 +1,83 @@
 import os
+import contextlib
+import subprocess
 
 # postinst
-#PACKAGE_GROUP = "djdd"
 #apt-get install debootstrap schroot
-#addgroup $PACKAGE_GROUP
+#addgroup djdd
 
 
-def initialize_build_directory(name, dir, clones, debian_suite, debian_arch, debian_mirror=None):
-    # SUDO
-    #adduser `whoami` djdd
+@contextlib.contextmanager
+def chroot(root_dir):
+    chroot_session = subprocess.check_output(['schroot', '--begin-session'])
+    cmd_schroot = ['schroot', '--chroot', chroot_session, '--run-session', root_dir]
+    def call(cmd, shell=False):
+        if shell:
+            full_cmd = ' '.join(cmd_schroot) + ' ' + cmd
+        else:
+            full_cmd = cmd_schroot + cmd
+        subprocess.call(full_cmd, shell=shell)
+    yield call
+    subprocess.call(['schroot', '-chroot', chroot_session, '--end-session'])
 
+
+def initialize_build_directory(name, dir, debian_suite, suite, debian_arch, clone, debian_mirror=None, sudo=True):
     log_dir = os.path.join(dir, "logs")
     root_dir = os.path.join(dir, "debootstrap_root")
     schroot_config_file = os.path.join(dir, "schroot.conf")
+    schroot_config_link = os.path.join("/etc/schroot/chroot.d", name+".conf")
+    link_marker = os.path.join(dir, "WAITING_FOR_LINK")
+    is_restart = os.path.exists(schroot_config_link) or (os.path.exists(link_marker) and sudo)
 
     os.makedirs(log_dir)
     os.makedirs(root_dir)
 
-    # Is there a reason why only this directory is created?
-    #internal_cache_dir = os.path.join(root_dir, "var/local/cache/djdd")
-    #os.makedirs(internal_cache_dir)
+    if not is_restart:
+        # Create the debootstrap
+        cmd_debootstrap = ['debootstrap', '--arch', debian_arch, debian_suite, root_dir]
+        if debian_mirror is not None:
+            cmd_debootstrap.append(debian_mirror)
+        subprocess.call(cmd_debootstrap)
 
-    # Create the debootstrap
-    debootstrap_cmd = ['debootstrap', '--arch', debian_arch, debian_suite, root_dir]
-    if debian_mirror is not None:
-        debootstrap_cmd.append(debian_mirror)
-    subprocess.call(debootstrap_cmd)
+        # Create schroot config for our build directory
+        schroot_config = """
+[{name}]
+description=djdd build {name}
+type=directory
+directory={dir}
+groups=djdd
+root-groups=root
+run-exec-scripts=false
+run-setup-scripts=false
+        """.format(name=name, dir=root_dir).strip()
+        with open(schroot_config_file, 'w') as f:
+            f.write(schroot_config)
 
-    # Setup schroot
-    schroot_config = """
-    [{name}]
-    description=djdd build {name}
-    type=directory
-    directory={dir}
-    groups=djdd
-    root-groups=root
-    run-exec-scripts=false
-    run-setup-scripts=false
-    """.format(name=name, dir=root_dir)
-    with open(schroot_config_file, 'w') as f:
-        f.write(schroot_config)
-    # XXX AS ROOT
-    #ln -s schroot_config_file schroot_config_link
+        # Only try with sudo if asked to
+        cmd_link = ["ln", "-s", schroot_config_file, schroot_config_link]
+        if sudo:
+            subprocess.call(["sudo"] + cmd_link)
+        else:
+            print("Run the following as root and retry: {}\n".format(" ".join(cmd_link)))
+            open(link_marker, 'wa').close()
+            sys.exit(1)
 
-    # Tidy up some aspects of the system (as root)
-    # XXX AS ROOT
-    if debian_mirror is not None:
-        with open(os.path.join(root_dir, 'etc/apt/sources.list'), 'w') as f:
-            f.write("deb {} {} main\n".format(debian_mirror, debian_suite))
-    # XXX AS ROOT
-    with open(os.path.join(root_dir, 'sbin/start-stop-daemon'), 'w') as f:
-        f.write("exit 0\n")
-    # XXX AS ROOT
-    with open(os.path.join(root_dir, 'etc/locale.gen'), 'w') as f:
-        f.write("en_US ISO-8859-1\nen_US.UTF-8 UTF-8\n")
+    # If the marker is set, but the file exists, then remove the marker and continue
+    if os.path.exists(link_marker) and os.path.exists(schroot_config_link):
+        os.remove(link_marker)
 
-    # Install everything we need
-    dependencies = [ 'locales', 'python-pip', 'python-virtualenv', 'git-buildpackage', 'python-jinja2']
-    dependencies += [ 'debhelper', 'build-essential', 'libbz2-dev', 'libevent-dev', 'libgeos-dev', 'libpq-dev', 'libsqlite3-dev', 'python-dev', 'libpng12-dev', 'libxml2', 'libxml2-dev', 'libxslt-dev', 'libncurses5-dev', 'git', 'git-core', 'mercurial', 'postgresql-server-dev-9.1', 'postgresql-9.1', 'libxml2-dev', 'libproj-dev', 'libjson0-dev', 'libgeos-dev', 'xsltproc', 'docbook-xsl', 'docbook-mathml', 'python2.7' ]
-    # XXX subprocess? single line in shell mode?
-    #schroot /var/local/lokaler-build "apt-get update && apt-get install $dependencies"
-    #SESSION=`schroot --begin-session`
-    #schroot --chroot "$SESSION" --run-session /var/local/lokaler-build apt-get update
-    #schroot --chroot "$SESSION" --run-session /var/local/lokaler-build apt-get install $dependencies
-    #schroot --chroot "$SESSION" --end-session
+    if schroot_config_link:
+        dependencies = [ 'locales', 'python-pip', 'python-virtualenv', 'git-buildpackage', 'python-jinja2']
+        dependencies += [ 'debhelper', 'build-essential', 'libbz2-dev', 'libevent-dev', 'libgeos-dev', 'libpq-dev', 'libsqlite3-dev', 'python-dev', 'libpng12-dev', 'libxml2', 'libxml2-dev', 'libxslt-dev', 'libncurses5-dev', 'git', 'git-core', 'mercurial', 'postgresql-server-dev-9.1', 'postgresql-9.1', 'libxml2-dev', 'libproj-dev', 'libjson0-dev', 'libgeos-dev', 'xsltproc', 'docbook-xsl', 'docbook-mathml', 'python2.7' ]
+
+        with chroot(root_dir) as call:
+            # Tidy up some aspects of the system (as root)
+            if debian_mirror is not None:
+                cmd = 'echo "deb {} {} main" > /etc/apt/sources.list'
+                call(cmd.format(debian_mirror, debian_suite), shell=True)
+            call('echo "exit 0" > /sbin/start-stop-daemon', shell=True)
+            call('echo "en_US ISO-8859-1\nen_US.UTF-8 UTF-8" > /etc/locale.gen', shell=True)
+            # Install anything we need
+            call('apt-get update', shell=True)
+            call(['apt-get', 'install'] + dependencies)
+
